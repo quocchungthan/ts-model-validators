56e14f2c2578cea222700eb6cab31da93de5f56f
Chung Than Quoc <chung.thanquoc@axonactive.com>
2021-01-11 14:12:49 +0700

    literally browser

--- karma.conf.js
+++ karma.conf.js
@@ -64,13 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeNoSandbox'],
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
-    },
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
